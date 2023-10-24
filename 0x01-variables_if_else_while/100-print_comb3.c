dabca9d4536128ba403753ae27a391e9087dc528
SaraHamdy01 <sara.hamdyofficial@gmail.com>
2023-10-24 08:39:44 -0700

    finished

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,22 +3,34 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints all possible combinations of single-digit numbers in
- *	        ascending order, separated by commas and spaces.
+ * Description: This program generates and prints all possible combinations
+ *	        of two digits satisfying the following conditions:
+ *	        - The digits must be different.
+ *	        - The smallest combination of two digits is printed.
+ *	        - Numbers are printed in ascending order with two digits.
+ *	        - Each number is separated by a comma and a space.
+ *	        - The putchar function is used for output, and no other
+ *		  output functions are allowed.
+ *	        - The code uses a maximum of five putchar function calls.
+ *	        - No variables of type char are used.
  *
  * Return: Always returns 0 to indicate successful execution
  */
 int main(void)
 {
-	int d;
+	int d1, d2;
 
-	for (d = '0'; d <= '9'; d++)
+	for (d1 = '0'; d1 <= '9'; d1++)
 	{
-		putchar(d);
-		if (d != '9')
+		for (d2 = d1 + 1; d2 <= '9'; d2++)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(d1);
+			putchar(d2);
+			if (d1 < '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
